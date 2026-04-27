ab332e5def50ec94c9f3f38ca8d524dd5e060cb7
yuvraj7000 <yuvraj7000raju@gmail.com>
2026-04-27 10:01:53 +0530

    basic collision part is completed

--- scripts/game/fire.gd
+++ scripts/game/fire.gd
@@ -7,6 +7,18 @@ func _ready():
 	print("fire : " + str(global_position))
 
 func _physics_process(delta):
-	
 	velocity = direction * speed
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var body = collision.get_collider()
+		
+		if body != null:
+			print("Collided with: ", body.name)
+			
+			# 🔥 destroy the object you hit
+			body.queue_free()
+			
+			# destroy bullet itself
+			queue_free()
