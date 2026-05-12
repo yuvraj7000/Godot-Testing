c35af444954e0afafaf1f32aeae86496807cfb3a
yuvraj7000 <yuvraj7000raju@gmail.com>
2026-05-12 12:58:34 +0530

    fixed blast animation strip 2d

--- scripts/game/fire.gd
+++ scripts/game/fire.gd
@@ -3,22 +3,47 @@ extends CharacterBody2D
 var speed = 800
 var direction = Vector2.ZERO
 
-func _ready():
-	print("fire : " + str(global_position))
+const BLAST_SCENE = preload("res://scenes/game/blast.tscn")
+
+var exploded := false
 
 func _physics_process(delta):
+	if exploded:
+		return
+	
 	velocity = direction * speed
 	move_and_slide()
-	
-	for i in get_slide_collision_count():
+
+	for i in range(get_slide_collision_count()):
 		var collision = get_slide_collision(i)
 		var body = collision.get_collider()
+
+		if body == null:
+			continue
+
+		exploded = true
+
+		print("Collided with: ", body.name)
+
+		# Create blast
+		var blast = BLAST_SCENE.instantiate()
+		blast.play("tank")
+		blast.global_position = global_position
 		
-		if body != null:
-			print("Collided with: ", body.name)
-			
-			# 🔥 destroy the object you hit
+		
+		# Add blast safely
+		get_parent().add_child(blast)
+
+		# Destroy enemy
+		if body.name != "stone":
 			body.queue_free()
-			
-			# destroy bullet itself
-			queue_free()
+		else:
+			print("stone is hitted")
+
+		# Hide bullet immediately
+		hide()
+
+		# Remove bullet after current frame
+		call_deferred("queue_free")
+
+		break
